6b89017e467bd359d81d17410ee20b7d2d8e9c94
Michael H <56214343+RiskyMH@users.noreply.github.com>
2023-03-29 09:10:01 +1100

    add image generate bot's icon

--- website/components/Logo.tsx
+++ website/components/Logo.tsx
@@ -3,7 +3,7 @@ import { addBasePath } from "next/dist/client/add-base-path";
 export default function Logo(props) {
 	return (
 		props.variation === "imgen" ? (
-			<img src="https://cdn.discordapp.com/avatars/959340277385007154/b7863d0a698ea22585749b44bd971b6b.png?size=512" className="rounded-full" alt="Logo" {...props} width="278" height="241" />
+			<img src={addBasePath("/imggen_bot.svg")} className="rounded-full" alt="Logo" {...props} width="278" height="241" />
 		) : (
 			<img src={addBasePath("/robot.svg")} alt="Logo" {...props} width="278" height="241" />
 		)
